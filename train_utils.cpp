c21db5380d8d1acb69f7914e91978493b9827570
Neeha  Sajja <neeha9chowdary@gmail.com>
2023-03-10 14:16:30 +0100

    Update train_utils.cpp

--- train_utils.cpp
+++ train_utils.cpp
@@ -1,23 +1,78 @@
-#include <torch/torch.h>
 #include <iostream>
-#include <map>
 #include <vector>
+#include <map>
+#include <tuple>
+ 
 
-using std::map;
-using std::vector;
+namespace argparse {
+    // define Namespace class
+    class Namespace {
+    public:
+        std::vector<int> selected_frames;
+        int bar;
 
-torch::Tensor sample_trajectories_batch(torch::nn::Module& model, torch::Tensor context, torch::Device device, std::map<std::string, std::map<std::string, torch::Tensor>> cfg) {
-    int n_samples = cfg["extractor_cfg"]["n_samples"].item<int>();
-    int n_time_steps = cfg["model_params"]["future_num_frames"].item<int>();
-    int bs = context.size(0).item<int>();
-    torch::Tensor samples = torch::zeros({bs, 1, n_samples, 2 * n_time_steps}, torch::dtype(torch::kFloat32).device(device));
-    for (int i = 0; i < n_samples; i++) {
-        torch::Tensor z = torch::randn({bs, cfg["cvae_cfg"]["latent_dim"].item<int>()}, torch::dtype(torch::kFloat32).device(device));
-        {
-            torch::NoGradGuard no_grad;
-            torch::Tensor trajectories = model.attr("inference")(z, context).toTensor();
-            samples.index_put_({torch::indexing::Slice(), 0, i, torch::indexing::Slice()}, trajectories);
-        }
+        Namespace(std::vector<int> selected_frames_, int bar_) :
+            selected_frames(selected_frames_), bar(bar_) {}
+    };
+
+    // define function to parse arguments
+    Namespace parse_args(int argc, char* argv[]) {
+        // create argument parser and add arguments
+        auto parser = argparse::ArgumentParser("Visualize argparse arguments.");
+        parser.add_argument("selected_frames")
+              .help("selected frames")
+              .nargs("+")
+              .action([](const std::string& value) {
+                return std::stoi(value);
+              });
+
+        parser.add_argument("-b", "--bar")
+              .help("the number of bar on the output.")
+              .set_default(50)
+              .action([](const std::string& value) {
+                return std::stoi(value);
+              });
+
+        // parse arguments
+        auto args = parser.parse_args(argc, argv);
+
+        // create Namespace object from parsed arguments
+        auto ns = argparse::Namespace(args.get<std::vector<int>>("selected_frames"),
+                                      args.get<int>("bar"));
+
+        return ns;
     }
-    return samples;
+
+    // define function to print argparse arguments
+    void print_argparse_arguments(const argparse::Namespace& p) {
+        std::cout << "PARAMETER SETTING" << std::endl;
+        std::cout << std::string(p.bar, '-') << std::endl;
+        std::map<std::string, std::tuple<int, std::vector<int>>> args = {
+            {"selected_frames", std::make_tuple(p.selected_frames.size(), p.selected_frames)},
+            {"bar", std::make_tuple(1, std::vector<int>{p.bar})}
+        };
+
+        for (const auto& arg : args) {
+            std::cout << std::setw(25) << std::left << arg.first << ": ";
+            auto arg_tuple = arg.second;
+            int size = std::get<0>(arg_tuple);
+            auto values = std::get<1>(arg_tuple);
+
+            if (size == 1) {
+                std::cout << values[0];
+            } else {
+                std::cout << std::endl;
+                for (int i = 0; i < size; i++) {
+                    std::cout << std::string(8, ' ') << values[i] << std::endl;
+                }
+            }
+        }
+        std::cout << std::string(p.bar, '-') << std::endl;
+    }
+}
+
+int main(int argc, char* argv[]) {
+    auto args = argparse::parse_args(argc, argv);
+    argparse::print_argparse_arguments(args);
+    return 0;
 }
