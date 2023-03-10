1a58876f0b5b166843e85f0372ca6f529e6dd122
Neeha  Sajja <neeha9chowdary@gmail.com>
2023-03-10 12:41:24 +0100

    Update losses.hpp

--- losses.hpp
+++ losses.hpp
@@ -1,47 +1,79 @@
 #include <torch/torch.h>
 #include <cmath>
 #include <vector>
+#include <iostream>
 
-float loss_KLD(torch::Tensor mean, torch::Tensor log_var, std::map<std::string, std::map<std::string, float>> cfg, bool batch_mean = true) {
-    float betta = cfg["cvae_cfg"]["betta"];
-    float KLD = -0.5 * torch::sum(1 + log_var - mean.pow(2) - log_var.exp()).item<float>();
-    if (batch_mean) {
-        int bs = mean.size(0);
-        KLD = KLD / bs;
-    }
-    return betta * KLD / mean.size(0);
-}
+torch::Tensor pytorch_neg_multi_log_likelihood_batch(
+    torch::Tensor gt,
+    torch::Tensor pred,
+    torch::Tensor confidences,
+    torch::Tensor avails,
+    float epsilon = 1.0e-8,
+    bool is_reduce = true
+) {
+    TORCH_CHECK(pred.dim() == 4, "expected 3D (MxTxC) array for pred, got ", pred.sizes());
+    auto [batch_size, num_modes, future_len, num_coords] = pred.sizes();
 
-torch::Tensor compute_kernel(torch::Tensor x, torch::Tensor y) {
-    int x_size = x.size(0);
-    int y_size = y.size(0);
-    int dim = x.size(1);
-    x = x.unsqueeze(1);  // (x_size, 1, dim)
-    y = y.unsqueeze(0);  // (1, y_size, dim)
-    auto tiled_x = x.expand({x_size, y_size, dim});
-    auto tiled_y = y.expand({x_size, y_size, dim});
-    auto kernel_input = (tiled_x - tiled_y).pow(2).mean(2)/static_cast<float>(dim);
-    return torch::exp(-kernel_input);  // (x_size, y_size)
-}
+    TORCH_CHECK(
+        gt.sizes() == torch::IntArrayRef{batch_size, future_len, num_coords},
+        "expected 2D (Time x Coords) array for gt, got ", gt.sizes()
+    );
 
-float compute_mmd(torch::Tensor x, torch::Tensor y, std::map<std::string, std::map<std::string, float>> cfg) {
-    float betta = cfg["cvae_cfg"]["betta"];
-    int z_dim = cfg["cvae_cfg"]["latent_dim"];
-    auto prior_samples = torch::randn({200, z_dim});
-    auto x_kernel = compute_kernel(x, x);
-    auto y_kernel = compute_kernel(y, y);
-    auto xy_kernel = compute_kernel(x, y);
-    auto mmd = x_kernel.mean() + y_kernel.mean() - 2*xy_kernel.mean();
-    return mmd * betta;
-}
+    TORCH_CHECK(
+        confidences.sizes() == torch::IntArrayRef{batch_size, num_modes},
+        "expected 1D (Modes) array for gt, got ", confidences.sizes()
+    );
 
-torch::Tensor neg_multi_log_likelihood_batch(torch::Tensor gt, torch::Tensor pred, torch::Tensor confidences, torch::Tensor avails) {
+    TORCH_CHECK(
+        torch::allclose(torch::sum(confidences, /*dim=*/1), torch::ones({batch_size}, confidences.options())),
+        "confidences should sum to 1"
+    );
+
+    TORCH_CHECK(
+        avails.sizes() == torch::IntArrayRef{batch_size, future_len},
+        "expected 1D (Time) array for gt, got ", avails.sizes()
+    );
+
+    TORCH_CHECK(torch::isfinite(pred).all(), "invalid value found in pred");
+    TORCH_CHECK(torch::isfinite(gt).all(), "invalid value found in gt");
+    TORCH_CHECK(torch::isfinite(confidences).all(), "invalid value found in confidences");
+    TORCH_CHECK(torch::isfinite(avails).all(), "invalid value found in avails");
+
     // convert to (batch_size, num_modes, future_len, num_coords)
     gt = gt.unsqueeze(1);  // add modes
     avails = avails.unsqueeze(1).unsqueeze(-1);  // add modes and cords
-    auto error = torch::sum(((gt - pred) * avails).pow(2), -1);  // reduce coords and use availability
-    error = torch::log(confidences) - 0.5 * torch::sum(error, -1);  // reduce time
-    auto max_value = std::get<0>(error.max(1, true));  // error are negative at this point, so max() gives the minimum one
-    error = -torch::log(torch::sum(torch::exp(error - max_value), -1)) - max_value;
-    return error.mean();
+
+    // error (batch_size, num_modes, future_len)
+    auto error = torch::sum(
+        (gt - pred).pow(2) * avails, /*dim=*/-1
+    );  // reduce coords and use availability
+
+    auto log_confidences = torch::log(confidences + epsilon);
+    auto negative_half_error = -0.5 * error;
+
+    auto likelihood = log_confidences + negative_half_error;
+    auto max_values = torch::max(likelihood, /*dim=*/1, /*keepdim=*/true);
+    auto exp_likelihood = torch::exp(likelihood - max_values.values);
+
+    auto log_likelihood = -torch::log(torch::sum(exp_likelihood, /*dim=*/1, /*keepdim=*/true)) - max_values.values;
+
+    if (is_reduce) {
+        return torch::mean(log_likelihood);
+    } else {
+        return log_likelihood;
+    }
+}
+
+torch::Tensor pytorch_neg_multi_log_likelihood_single(
+    torch::Tensor gt,
+    torch::Tensor pred,
+    torch::Tensor avails
+) {
+    // pred (bs)x(time)x(2D coords) --> (bs)x(mode=1)x(time)x(2D coords)
+    // create confidence (bs)x(mode=1)
+    auto [batch_size, future_len, num_coords] = pred.sizes();
+    auto confidences = pred.new_ones({batch_size, 1});
+    return pytorch_neg_multi_log_likelihood_batch(
+        gt, pred.unsqueeze(1), confidences, avails
+    );
 }
